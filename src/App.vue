82b8a8bb85e60f809654e48cfd0a9e1ff38cbfca
Brian Nelson <banman1985@gmail.com>
2020-12-04 09:06:54 -0600

    recommit test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello From Your Vue.js App"/>
+   <Header theTitle = "Popular TV Shows"/> 
+   <TVList  v-bind:tvCards="cards"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import TVList from './components/TVList.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    TVList
+
+  },
+  data(){
+  return {
+    tvList:[]
+  }
+  },
+  mounted()
+  {
+    axios.get('https://api.themoviedb.org/3/tv/popular?api_key=e72d704efa7872abba1106f277234689&language=en-US&page=1')
+    .then((res) => {
+      this.tvList = res.data.results;
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
